79148505d8f9dc01edd33eb38475a74eff785216
Vitor Lima <viitorlima2000@gmail.com>
2025-04-09 16:59:03 -0300

    venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/vitor/Desktop/parking_lot_app/venv
+set -gx VIRTUAL_ENV /home/vitor/parking_lot_app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
